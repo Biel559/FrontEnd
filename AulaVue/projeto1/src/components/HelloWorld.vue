43d7549fdb9677867b33cb83f65ab2f0983cd967
Biel559 <bielcampopiano@gmail.com>
2024-09-04 22:35:38 -0300

    Primeiro Commit
    Commit com as 3 aulas de frontend realizadas em sala de aula

---
+++ AulaVue/projeto1/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <p>{{ mensagem }}</p>
+  <p>Contador: {{ contador }}</p>
+  <p v-if="mostrar">Texto do Mostrar</p>
+    <button @click="incrementar">Clique para incrementar</button>
+    <button @click="reset">Reset</button>
+    <button @click="mostrando">Mostrar/Ocultar</button>
+    <input v-model="nome" placeholder="Digite seu nome">
+    <br>
+    <p>{{ texto }}</p>
+  <ul>
+   <li v-for="item in itens" :key="item.text">
+    {{ item.text }}
+   </li> 
+  </ul>
+
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      mensagem: "Bem-vindo ao Vue.js!",
+      contador: 0,
+      mostrar: false,
+      itens: [
+        { text:"Ponte Preta"},
+        { text:"Guarani"},
+        { text:"Novohorizontino"},
+        { text:"Ituano"},
+        { text:"Mirassol"},
+        { text:"RB Bragantino"}
+      ],
+      nome: "Gabriel",
+      texto: "Texto genéricão"
+    };
+  },
+  methods: {
+    incrementar() {
+      this.contador ++;
+    },
+    reset() {
+      this.contador = 0;
+    },
+    mostrando() {
+      this.mostrar = !this.mostrar;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+p {
+  color: green;
+}
+
+button {
+  margin: 10px;
+}
+</style>
