35da2d4ee213e78a6de46c0bb52080167c189588
Gabriel Campopiano Rosa <166143416+Biel559@users.noreply.github.com>
2024-10-04 13:30:38 -0300

    Update Book.js

--- AulaCrud/library-backend/models/Book.js
+++ AulaCrud/library-backend/models/Book.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Definindo o esquema do livro
 const BookSchema = new mongoose.Schema({
-     title: { type: String, requiered: true},
+     title: { type: String, required: true},
      author: { type: String, required: true},
      year: { type:Number },
 });
