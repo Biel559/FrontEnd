f5aefe0b6a24c05afe809354519b1a64b6e3ceed
Biel559 <bielcampopiano@gmail.com>
2024-12-09 22:42:17 -0300

    commit memoria

--- SomativaFrontSegundoSemestre/frontend/user-frontend/vue.config.js
+++ SomativaFrontSegundoSemestre/frontend/user-frontend/vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  productionSourceMap: false,
+};
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+    },
+  },
+};
